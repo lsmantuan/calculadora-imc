56a30a35952331761fb18dc8295ffaca33aa5da9
Lucas Mantuan <lucasmantuan@outlook.com>
2021-11-25 23:49:58 -0300

    Alterações Diversas

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var view = {
-    exibirResultado: function(msg) {
-        var resultado = document.getElementById("imc");
-        resultado.innerHTML = msg;
+    exibirResultado: function(imc) {
+        var resultadoIMC = document.getElementById("imc");
+        resultadoIMC.innerHTML = imc;
     },
     
-    exibirSobre: function(msg) {
-        var sobre = document.getElementById("sobre");
-        sobre.innerHTML = msg;
+    exibirSobre: function(titulo, sobre) {
+        var resultadoTitulo = document.getElementById("titulo");
+        var resultadoSobre = document.getElementById("sobre");
+        resultadoTitulo.innerHTML = titulo;
+        resultadoSobre.innerHTML = sobre;
     },
 
     exibirErro: function(erro) {
@@ -17,6 +19,11 @@ var view = {
     limparErro: function(erro) {
         var limparDigitacao = document.getElementById(erro);
         limparDigitacao.setAttribute("class", "campoPadrao");
+    },
+
+    alterarImagem: function(tipo) {
+        var imagem = document.getElementById("imagem");
+        imagem.style.backgroundImage = "url(imagens/" + tipo + ".png)";
     }
 };
 
@@ -29,15 +36,23 @@ var model = {
 
     processarSobre: function(imc) {
         if (imc < 18.5) {
-            view.exibirSobre("Abaixo do Peso");
+            view.exibirSobre("Abaixo do Normal", "Procure um médico. Algumas pessoas têm um baixo peso por características do seu organismo e tudo bem. Outras podem estar enfrentando problemas, como a desnutrição. É preciso saber qual é o caso.");
+            view.alterarImagem("abaixo");
         } else if (imc < 24.9) {
-            view.exibirSobre("Normal");
+            view.exibirSobre("Normal", "Que bom que você está com o peso normal! E o melhor jeito de continuar assim é mantendo um estilo de vida ativo e uma alimentação equilibrada.");
+            view.alterarImagem("normal");
         } else if (imc < 29.9) {
-            view.exibirSobre("Sobrepeso");
+            view.exibirSobre("Sobrepeso", "Ele é, na verdade, uma pré-obesidade e muitas pessoas nessa faixa já apresentam doenças associadas, como diabetes e hipertensão. Importante rever hábitos e buscar ajuda antes de, por uma série de fatores, entrar na faixa da obesidade pra valer.");
+            view.alterarImagem("sobrepeso");
+        } else if (imc < 34.9) {
+            view.exibirSobre("Obesidade Grau I", "Sinal de alerta! Chegou na hora de se cuidar, mesmo que seus exames sejam normais. Vamos dar início a mudanças hoje! Cuide de sua alimentação. Você precisa iniciar um acompanhamento com nutricionista e/ou endocrinologista.");
+            view.alterarImagem("obesidade");
         } else if (imc < 39.9) {
-            view.exibirSobre("Obesidade");
+            view.exibirSobre("Obesidade Grau II", "Mesmo que seus exames aparentem estar normais, é hora de se cuidar, iniciando mudanças no estilo de vida com o acompanhamento próximo de profissionais de saúde.");            
+            view.alterarImagem("obesidade");
         } else {
-            view.exibirSobre("Obesidade Grave")
+            view.exibirSobre("Obesidade Grau III", "Aqui o sinal é vermelho, com forte probabilidade de já existirem doenças muito graves associadas. O tratamento deve ser ainda mais urgente.")
+            view.alterarImagem("obesidade");
         };
     }
 };
@@ -45,18 +60,18 @@ var model = {
 var controller = {
     analisarPesoAltura: function(peso, altura) {
         var formatacaoPeso = new RegExp(/^\d{1,3}[.,]?\d{0,2}$/);
-        var resultadoFormatacaoPeso = peso.match(formatacaoPeso);
+        var resultadoFormatacaoPeso = formatacaoPeso.test(peso);
 
         var formatacaoAltura = new RegExp(/^[0-2]{1}[.,]?\d{0,2}$/);
-        var resultadoFormatacaoAltura = altura.match(formatacaoAltura);
+        var resultadoFormatacaoAltura = formatacaoAltura.test(altura);
         
-        if ((resultadoFormatacaoPeso !== null) && (resultadoFormatacaoAltura !== null)) {
+        if ((resultadoFormatacaoPeso === true) && (resultadoFormatacaoAltura === true)) {
             model.calcularResultado(peso, altura)
         } else {
-            if (resultadoFormatacaoPeso === null) {
+            if (resultadoFormatacaoPeso === false) {
                 view.exibirErro("peso");
             };
-            if (resultadoFormatacaoAltura === null) {
+            if (resultadoFormatacaoAltura === false) {
                 view.exibirErro("altura");
             };
         };        
@@ -77,14 +92,24 @@ function limparIMC() {
     var alturaInformada = document.getElementById("altura");
     alturaInformada.value = "";
     view.exibirResultado("0");
-    view.exibirSobre("O que seu IMC significa?");
+    view.exibirSobre("IMC", "Descubra o seu Índice de Massa Corporal (IMC) digitando seu peso (kg) e altura (m) e em seguida clicando no botão calcular!");
     view.limparErro("peso");
     view.limparErro("altura");
-    limparInput();
+    view.alterarImagem("inativo");
 };
 
-function limparInput() {
-    var inputAlvo = document.getElementsByClassName("normal");
-    inputAlvo[0].onfocus = "teste";
-    console.log(inputAlvo);
-}
+window.onload = function() { 
+    var input = document.getElementsByClassName("campoPadrao");
+    for (let i = 0; i < input.length; i++) {
+    input[i].onfocus = limparInput;
+    }
+};
+
+function limparInput(event) {
+    var classAlvo = event.target.className;
+    var idAlvo = event.target.id;
+    if (classAlvo === "campoErro") {
+        var input = document.getElementById(idAlvo);
+        input.setAttribute("class", "campoPadrao");        
+    }
+};
