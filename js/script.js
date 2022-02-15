6a08702217f68d1a39c37bf82fa9694ef594a690
Lucas Mantuan <lucasmantuan@outlook.com>
2022-02-14 22:50:19 -0300

    Alteração no RegExp do Peso

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ var model = {
 
 var controller = {
     analisarPesoAltura: function(peso, altura) {
-        var formatacaoPeso = new RegExp(/^[0-3][.,]?\d{0,2}$/);
+        var formatacaoPeso = new RegExp(/^[0-9][.,]?\d{0,2}$/);
         var resultadoFormatacaoPeso = formatacaoPeso.test(peso);
 
         var formatacaoAltura = new RegExp(/^[0-2]{1}[.,]?\d{0,2}$/);
